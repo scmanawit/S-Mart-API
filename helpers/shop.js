523dc4be64a91377d5b362e2be22a769f36d5fea
scmanawit <s.manawit@gmail.com>
2023-02-18 22:42:14 +0800

    shop helper functions

---
+++ helpers/shop.js
@@ -0,0 +1,23 @@
+import Shop from "../models/Shop.js"
+
+export const getShop = async (shopId) => {
+    return await Shop.findById(shopId).exec()
+}
+
+export const getShopBy = async (filters) => {
+    return await Shop.findOne(filters).exec()
+}
+
+export const getShopsBy = async (filters) => {
+    return await Shop.find(filters).exec()
+}
+
+export const saveShop = async (data, shopId = null) => {
+    if (shopId) {
+        return await Shop.findByIdAndUpdate(shopId, data, { new: true }).exec()
+    }
+
+    const shop = new Shop(data)
+    await shop.save()
+    return shop
+}
